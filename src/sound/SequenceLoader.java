a3495b976c065971dad33822b6cc67b45ab681a7
claarm15 <claarm15@students.ecu.edu>
2016-02-12 07:34:26 -0500

    Mario Tripplets working, need to work on sharps & flats through measure next

--- src/sound/SequenceLoader.java
+++ src/sound/SequenceLoader.java
@@ -15,7 +15,7 @@ public class SequenceLoader {
 	private int notesPerBeat;
 	private int v1RepeatPoint;
 	private int lastNoteDuration;
-	
+
 	public SequenceLoader(int tempo, ArrayList<String> songLines) throws MidiUnavailableException, InvalidMidiDataException {
 		startTick = 1;
 		notesPerBeat = 1;
@@ -32,8 +32,8 @@ public class SequenceLoader {
 			if (songLine.matches("^[CKLMQTXV]:.*$")) {
 				if (songLine.matches("^[vV]: *1")) {
 					v1RepeatPoint = startTick;
-				}
-				else if (songLine.matches("^[vV]:.*")) startTick = v1RepeatPoint;
+				} else if (songLine.matches("^[vV]:.*"))
+					startTick = v1RepeatPoint;
 			} else if (songLine.matches("[a-gzA-G0-9,:| \\[\\]\\/_^\\(']*")) {
 				processNotesInLine(songLine);
 			} else {
@@ -58,25 +58,34 @@ public class SequenceLoader {
 
 	private void processNotesInBeat(String beat) {
 		boolean in_chord = false;
+		int tupletSpec = 1;
+		int tupletCount = 0;
 		ArrayList<String> notes = splitBeatIntoNotes(beat);
 		for (String noteCode : notes) {
 			System.out.println("NOTE CODE: " + noteCode);
-			//if in a chord, startTick stays the same...
-			if (noteCode.matches("\\[")) in_chord = true;
+			// if in a chord, startTick stays the same...
+			if (noteCode.matches("\\["))
+				in_chord = true;
 			if (noteCode.matches("\\]")) {
 				in_chord = false;
 				startTick += lastNoteDuration;
 			}
-			
+			if (noteCode.matches("\\(\\d")) {
+				tupletCount = Integer.parseInt(noteCode.substring(1));
+				tupletSpec = tupletCount;
+			}
+
 			if (noteCode.matches(".*[A-Ga-g].*")) {
 				Note note = new Note(noteCode);
-				lastNoteDuration = note.getDuration();
+				lastNoteDuration = note.getDuration() / tupletSpec;
+				if (tupletCount > 0) tupletCount--;
+				else tupletSpec = 1;
 				int pitch = note.getPitch();
 				if (!in_chord) startTick += lastNoteDuration;
 				String noteLetter = note.getNoteLetter() + "";
-				if (!noteLetter.matches("[zZ]"))song.addNote(pitch, startTick, lastNoteDuration);
-				System.out.println(note.getNoteLetter() + " pitch: " + note.getPitch() + "\tOctave:  " + note.getOctave() + "\t NumTicks: "
-						+ lastNoteDuration + "\tstartTick: " + startTick );
+				if (!noteLetter.matches("[zZ]")) song.addNote(pitch, startTick, lastNoteDuration);
+				System.out.println(note.getNoteLetter() + " pitch: " + note.getPitch() + "\tOctave:  " + note.getOctave() + "\t NumTicks: " + lastNoteDuration
+						+ "\tstartTick: " + startTick);
 
 			}
 		}
@@ -93,13 +102,22 @@ public class SequenceLoader {
 			boolean somethingMatched = false;
 			boolean is_note = true;
 			String currentNote = "";
-			//If not a note capture symbols together, and separate from the notes...
+			// If not a note capture symbols together, and separate from the
+			// notes...
 			while (currentPosition < beatLength && beat.substring(currentPosition, currentPosition + 1).matches("[|:\\[\\]]")) {
 				currentNote += beat.substring(currentPosition, currentPosition + 1);
 				currentPosition++;
 				is_note = false;
 				somethingMatched = true;
 			}
+			if (currentPosition < beatLength - 1 && beat.substring(currentPosition, currentPosition + 2).matches("\\(\\d")) {
+				currentNote += beat.substring(currentPosition, currentPosition + 2);
+				currentPosition += 2;
+				is_note = false;
+				somethingMatched = true;
+
+			}
+
 			// Otherwise grab the note sequence
 			// get symbols before the letters
 			while (is_note && currentPosition < beatLength && beat.substring(currentPosition, currentPosition + 1).matches("[_^=]")) {
@@ -113,7 +131,7 @@ public class SequenceLoader {
 				currentPosition++;
 				somethingMatched = true;
 			}
-			//get after the note symbols.
+			// get after the note symbols.
 			while (is_note && currentPosition < beatLength && beat.substring(currentPosition, currentPosition + 1).matches("[',0-9/]")) {
 				currentNote += beat.substring(currentPosition, currentPosition + 1);
 				currentPosition++;
